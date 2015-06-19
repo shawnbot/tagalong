ce4d77a7e348e975e4e85c76926000ddfa1c3f1f
Shawn Allen <shawn.allen@gsa.gov>
2015-06-19 12:16:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,151 @@
+const ARRAY_TEMPLATE = '__array_template__';
+
+function bind(node, data, directives) {
+  if (typeof node === 'string') {
+    var selector = node;
+    node = document.querySelector(node);
+    if (!node) throw new Error('no such element: ' + selector);
+  }
+
+  if (typeof directives === 'object') {
+    directives = expandKeys(directives);
+    applyDirectives(node, data, directives);
+  } else if (typeof directives === 'function') {
+    directives = directives.call(node, data) || {};
+  } else {
+    directives = {};
+  }
+
+  if (Array.isArray(data)) {
+    return bindArray(node, data, directives);
+  } else if (typeof data === 'object') {
+    return bindObject(node, data, directives);
+  }
+
+  return bindScalar(node, data, directives);
+}
+
+function bindObject(node, data, directives) {
+  for (var key in data) {
+    if (!data.hasOwnProperty(key)) continue;
+    bindKey(node, key, data[key], directives[key]);
+  }
+  return node;
+}
+
+function bindArray(node, data, directives) {
+  var nodes = getChildren(node);
+  var template = node[ARRAY_TEMPLATE] || (node[ARRAY_TEMPLATE] = nodes[0]);
+  var nodeCount = nodes.length;
+  var dataCount = data.length;
+  if (nodeCount < dataCount) {
+    var last = nodes[nodeCount - 1] || template;
+    while (nodeCount < dataCount) {
+      var clone = template.cloneNode(true);
+      nodeCount = nodes.push(node.insertBefore(clone, last.nextSibling));
+      last = clone;
+    }
+  } else if (nodeCount > dataCount) {
+    while (nodeCount > dataCount) {
+      var child = nodes.shift();
+      node.removeChild(child);
+      nodeCount--;
+    }
+  }
+  for (var i = 0; i < dataCount; i++) {
+    bind(nodes[i], data[i], directives);
+  }
+  return node;
+}
+
+function bindKey(node, key, value, directives) {
+  var target = findKeyNode(node, key);
+  if (!target) {
+    return false;
+  }
+  return bind(target, value, directives);
+}
+
+function bindScalar(node, data, directives) {
+  var stringify = directives.stringify || String;
+  node.textContent = stringify.call(node, data);
+  return node;
+}
+
+function findKeyNode(node, key) {
+  var classed = node.getElementsByClassName(key)[0];
+  if (classed) return classed;
+
+  // Doing it this way, rather than:
+  //
+  // `querySelector('[data-bind="' + key + '"]')`,
+  //
+  // ...ensures that we won't get an exception for keys that would produce
+  // invalid selectors.
+  var children = node.querySelectorAll('[data-bind]');
+  for (var i = 0, len = children.length; i < len; i++) {
+    if (children[i].getAttribute('data-bind') == key) {
+      return children[i];
+    }
+  }
+  return null;
+}
+
+function getChildren(node) {
+  var children = [];
+  for (var child = node.firstChild; child; child = child.nextSibling) {
+    if (child.nodeType === 1) {
+      children.push(child);
+    }
+  }
+  return children;
+}
+
+function access(getter, data, thisArg) {
+  if (typeof getter === 'function') {
+    return getter.call(thisArg, data);
+  }
+  return data[getter];
+}
+
+function applyDirectives(node, data, directives) {
+  var interpolated;
+  for (var key in directives) {
+    if (key.charAt(0) === '@') {
+      var attr = key.substr(1);
+      var value = directives[key] === true
+        ? data[attr]
+        : access(directives[key], data, node);
+      node.setAttribute(attr, value);
+    } else {
+      // only interpolate scalars and functions;
+      // nested directives apply to children
+      if (typeof directives[key] !== 'object') {
+        if (!interpolated) interpolated = {};
+        interpolated[key] = access(directives[key], data, node);
+      }
+    }
+  }
+  if (interpolated) {
+    bind(node, interpolated);
+  }
+}
+
+function expandKeys(obj) {
+  var expanded = {};
+  for (var key in obj) {
+    if (key.indexOf('.') > 0) {
+      var bits = key.split('.');
+      var value = obj[key];
+      var d = expanded;
+      while (bits.length) {
+        d = d[bits.shift()] = bits.length ? {} : value;
+      }
+    } else {
+      expanded[key] = obj[key];
+    }
+  }
+  return expanded;
+}
+
+module.exports = bind;
---
+++ test/spec.js
@@ -0,0 +1,200 @@
+var bind = require('../');
+var assert = require('assert');
+var jsdom = require('jsdom');
+
+describe('data binding', function() {
+
+  var window, document;
+  beforeEach(function(done) {
+    jsdom.env('<body></body>', function(error, _window) {
+      if (error) return done(error);
+      window = _window;
+      document = window.document;
+      return done();
+    });
+  });
+
+  afterEach(function() {
+    window.close();
+    window = document = null;
+  });
+
+  describe('scalar binding', function() {
+    it('binds a scalar to element text', function() {
+      var el = document.createElement('span');
+      bind(el, 'foo');
+      assert.equal(el.textContent, 'foo');
+    });
+  });
+
+  describe('object binding', function() {
+
+    it('binds a key to a classed element', function() {
+      var el = document.createElement('div');
+      var span = el.appendChild(document.createElement('span'));
+      span.className = 'foo';
+      bind(el, {foo: 'bar'});
+      assert.equal(span.textContent, 'bar');
+    });
+
+    it('binds a key to an element with data-bind="key"', function() {
+      var el = document.createElement('div');
+      var span = el.appendChild(document.createElement('span'));
+      span.setAttribute('data-bind', 'foo');
+      bind(el, {foo: 'bar'});
+      assert.equal(span.textContent, 'bar');
+    });
+
+    it('binds a key to an element with data-bind="Some Key"', function() {
+      var el = document.createElement('div');
+      var span = el.appendChild(document.createElement('span'));
+      span.setAttribute('data-bind', 'Some Key');
+      bind(el, {'Some Key': 'bar'});
+      assert.equal(span.textContent, 'bar');
+    });
+
+    describe('@attribute setting', function() {
+
+      it('binds attributes with "@attr" directives as strings', function() {
+        var div = document.createElement('div');
+        bind(div, {foo: 'bar'}, {'@id': 'foo'});
+        assert.equal(div.id, 'bar');
+      });
+
+      it('binds attributes with "@attr" directives as functions', function() {
+        var div = document.createElement('div');
+        bind(div, {foo: 'bar'}, {'@id': function(d) { return d.foo + '_'; }});
+        assert.equal(div.id, 'bar_');
+      });
+
+      it('binds attributes with "@attr" directives as `true`', function() {
+        var div = document.createElement('div');
+        bind(div, {id: 'bar'}, {'@id': true});
+        assert.equal(div.id, 'bar');
+      });
+
+      it('binds nested attributes with "@attr" directives', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        bind(div, {x: {foo: 'bar'}}, {x: {'@id': 'foo'}});
+        assert.equal(span.id, 'bar');
+        assert.equal(span.textContent, '');
+      });
+
+      it('understands nested attribute directives (`x.@id`)', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        bind(div, {x: {foo: 'bar'}}, {'x.@id': 'foo'});
+        assert.equal(span.id, 'bar');
+        assert.equal(span.textContent, '');
+      });
+
+    }); // @attributes setting
+
+    describe('directive interpolation', function() {
+
+      it('interpolates key strings from directives', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        bind(div, {y: 'foo'}, {x: 'y'});
+        assert.equal(span.textContent, 'foo');
+      });
+
+      it('interpolates object-nested directives ({x:y:{}})', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        var i = span.appendChild(document.createElement('i'));
+        i.className = 'y';
+        bind(div, {x: {foo: 'bar'}}, {x: {y: 'foo'}});
+        assert.equal(i.textContent, 'bar');
+      });
+
+      it('interpolates string-nested directives (`x.y`)', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        var i = span.appendChild(document.createElement('i'));
+        i.className = 'y';
+        bind(div, {x: {foo: 'bar'}}, {'x.y': 'foo'});
+        assert.equal(i.textContent, 'bar');
+      });
+
+      it('interpolates functions as directives', function() {
+        var div = document.createElement('div');
+        var span = div.appendChild(document.createElement('span'));
+        span.className = 'x';
+        bind(div, {y: 'foo'}, {x: function(d) { return d.y; }});
+        assert.equal(span.textContent, 'foo');
+      });
+
+    }); // directive interpolation
+
+  }); // object binding
+
+  describe('array binding', function() {
+
+    it("binds an array to a node's children", function() {
+      var ul = document.createElement('ul');
+      var li = ul.appendChild(document.createElement('li'));
+      bind(ul, ['foo', 'bar']);
+      var lis = ul.querySelectorAll('li');
+      assert.equal(lis.length, 2);
+      assert.equal(lis[0].textContent, 'foo');
+      assert.equal(lis[1].textContent, 'bar');
+    });
+
+    it('binds a array by property name', function() {
+      var div = document.createElement('div');
+      var ul = div.appendChild(document.createElement('ul'));
+      ul.className = 'items';
+      var li = ul.appendChild(document.createElement('li'));
+      bind(div, {items: ['foo', 'bar']});
+      var lis = ul.querySelectorAll('li');
+      assert.equal(lis.length, 2);
+      assert.equal(lis[0].textContent, 'foo');
+      assert.equal(lis[1].textContent, 'bar');
+    });
+
+    it('binds a array by property name and object key', function() {
+      var div = document.createElement('div');
+      var ul = div.appendChild(document.createElement('ul'));
+      ul.className = 'items';
+      var li = ul.appendChild(document.createElement('li'));
+      li.appendChild(document.createElement('span')).className = 'value';
+      bind(div, {
+        items: [
+          {value: 'foo'},
+          {value: 'bar'}
+        ]
+      });
+      var lis = ul.querySelectorAll('li');
+      assert.equal(lis.length, 2);
+      assert.equal(lis[0].firstChild.textContent, 'foo');
+      assert.equal(lis[1].firstChild.textContent, 'bar');
+    });
+
+    it('can bind to empty arrays, then non-empty ones', function() {
+      var div = document.createElement('div');
+      var ul = div.appendChild(document.createElement('ul'));
+      ul.className = 'items';
+      var li = ul.appendChild(document.createElement('li'));
+      li.appendChild(document.createElement('span')).className = 'value';
+
+      var data = {items: []};
+      bind(div, data);
+      data.items = [{value: 'x'}, {value: 'y'}];
+      bind(div, data);
+
+      var lis = ul.querySelectorAll('li');
+      assert.equal(lis.length, 2);
+      assert.equal(lis[0].firstChild.textContent, 'x');
+      assert.equal(lis[1].firstChild.textContent, 'y');
+    });
+
+  }); // array binding
+
+}); // bind
