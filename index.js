a5942ed064c73fb8ab9e5f7bd7249409aedb7e1d
Shawn Allen <shawn.allen@gsa.gov>
2015-06-19 23:19:10 -0700

    add some examples

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
 
   var change = (function(ms) {
     var timeout;
-    return function() {
+    return function(force) {
       if (timeout) clearTimeout(timeout);
       timeout = setTimeout(function() {
         // console.log('rendering...');
@@ -30,13 +30,99 @@
           console.error(error);
         }
         outputHTML.textContent = template.outerHTML;
-      }, ms);
+      }, force === true ? 10 : ms);
     };
-  })(500);
+  })(200);
 
   inputHTML.on('change', change);
   inputJS.on('change', change);
 
   change();
+
+  var multiline = function(fn) {
+    return fn.toString()
+      .trim()
+      .replace(/^function\s*\(\)\s*{\s*\/\*\s*/, '')
+      .replace(/\s*\*\/}$/, '');
+  };
+
+  var examples = [
+
+    {
+      id: 'list',
+      title: 'List',
+      template: multiline(function(){/*
+<ul id="template">
+  <li>
+    <span class="first"></span>
+    <span class="last"></span>
+  </li>
+</ul>
+      */}),
+      script: multiline(function(){/*
+tagalong('#template', [
+  {first: 'Jill', last: 'Hughes'},
+  {first: 'Jack', last: 'Adams'},
+  {first: 'Jo', last: 'Weaver'}
+]);
+      */})
+    },
+
+    {
+      id: 'table',
+      title: 'Table',
+      template: multiline(function(){/*
+<table id="template">
+  <caption class="title"></caption>
+  <thead>
+    <tr class="columns">
+      <th></th>
+    </tr>
+  </thead>
+  <tbody class="rows">
+    <tr>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+      */}),
+      script: multiline(function(){/*
+tagalong('#template', {
+  columns: ['name', 'age', 'occupation'],
+  rows: [
+    ['Judy', 28, 'Developer'],
+    ['Jackie', 32, 'Designer'],
+    ['Rosie', 30, 'Riveter']
+  ]
+});
+      */})
+    }
+
+  ];
+
+  var ex = tagalong('#examples', examples, {
+    '@href': function(d) { return '#' + d.id; },
+    '@data-template': 'template',
+    '@data-script': 'script'
+  });
+
+  var selectedLink;
+  var hashchange = function() {
+    if (selectedLink) selectedLink.classList.remove('active');
+    var link = document.querySelector('[href="' + location.hash + '"]');
+    if (link) {
+      link.classList.add('active');
+      // console.log('activating example:', link);
+      inputHTML.setValue(link.getAttribute('data-template'));
+      inputJS.setValue(link.getAttribute('data-script'));
+      selectedLink = link;
+      change(true);
+    } else {
+      // console.log('no such example:', location.hash);
+    }
+  };
+
+  window.addEventListener('hashchange', hashchange);
+  if (location.hash) hashchange();
 
 })();
