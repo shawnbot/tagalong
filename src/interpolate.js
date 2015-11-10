9417b9f9267b9e7ef456b35e70bd8b5944df4673
Shawn Allen <shawn.allen@gsa.gov>
2015-11-10 15:57:37 -0800

    add basic string template interpolator

---
+++ src/interpolate.js
@@ -0,0 +1,10 @@
+var evaluate = require('./evaluate').evaluate;
+
+module.exports = interpolate;
+
+function interpolate(template, data) {
+  var that = this;
+  return template.replace(/{{(.+)}}/g, function(_, expr) {
+    return evaluate.call(that, expr, data);
+  });
+}
