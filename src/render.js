dad1fa2475b3a123c7366b535e761c2441b49882
Shawn Allen <shawn.allen@gsa.gov>
2015-11-10 13:44:07 -0800

    add note about DOM matches() compatiblity

--- src/render.js
+++ src/render.js
@@ -227,6 +227,8 @@ function interpolateAttributes(attrMap, data) {
 function getPreviousSibling(node, selector) {
   while (node = node.previousSibling) {
     if (!node) break;
+    // FIXME this needs a vendor prefix in IE 9+
+    // <http://caniuse.com/#search=matches>
     if (node.matches(selector)) return node;
   }
   throw new Error('no previous sibling found matching: ' + selector);
