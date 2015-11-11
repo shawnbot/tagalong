c69d339c0f3c4ae63a8eb7af03702850fc7ece2b
Shawn Allen <shawn.allen@gsa.gov>
2015-11-10 16:16:10 -0800

    add interpolate.isTemplate()

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,10 +1,15 @@
 var evaluate = require('./evaluate').evaluate;
+var pattern = /{{([^}]+)}}/g;
 
 module.exports = interpolate;
 
+module.exports.isTemplate = function(template) {
+  return !!template.match(pattern);
+};
+
 function interpolate(template, data) {
   var that = this;
-  return template.replace(/{{([^}]+)}}/g, function(_, expr) {
+  return template.replace(pattern, function(_, expr) {
     return evaluate.call(that, expr, data);
   });
 }
