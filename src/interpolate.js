71f09a3ed7d95e78f3ffcd60bdc750235126d29f
Shawn Allen <shawn.allen@gsa.gov>
2016-05-20 14:08:16 -0700

    rewrite interpolation, add tests

--- src/interpolate.js
+++ src/interpolate.js
@@ -1,15 +1,41 @@
 var evaluate = require('./evaluate').evaluate;
-var pattern = /{{([^}]+)}}/g;
+
+var functor = function(x) {
+  return function() {
+    return x;
+  }
+};
+
+var pattern = /{{\s*([^}]+)\s*}}/g;
+
+var isTemplate = function(str) {
+  return new RegExp(pattern).test(str);
+};
+
+var compile = function(template) {
+  if (typeof template !== 'string') {
+    throw new Error('interpolate.compile() expected a string;' +
+                    'got ' + (typeof template));
+  }
+
+  if (!isTemplate(template)) {
+    return functor(template);
+  }
+
+  return function(data) {
+    var that = this;
+    return template.replace(pattern, function(_, part) {
+      return evaluate.call(that, part, data);
+    });
+  };
+};
+
+var interpolate = function interpolate(str, data) {
+  return compile(str).call(this, data);
+};
 
 module.exports = interpolate;
 
-module.exports.isTemplate = function(template) {
-  return !!template.match(pattern);
-};
+module.exports.isTemplate = isTemplate;
 
-function interpolate(template, data) {
-  var that = this;
-  return template.replace(pattern, function(_, expr) {
-    return evaluate.call(that, expr, data);
-  });
-}
+module.exports.compile = compile;
