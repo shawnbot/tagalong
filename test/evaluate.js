a588f47f6cd59b4ceb8be0c2998ae1739c15b665
Shawn Allen <shawn.allen@gsa.gov>
2016-05-23 11:58:16 -0700

    add some evaluate tests

---
+++ test/evaluate.js
@@ -0,0 +1,82 @@
+var e = require('../src/evaluate');
+var assert = require('assert');
+
+describe('evaluator()', () => {
+  it('returns a function', () => {
+    assert.equal(typeof e.evaluator('x'), 'function');
+    assert.equal(typeof e.evaluator('x + 1'), 'function');
+  });
+});
+
+describe('evaluate()', () => {
+
+  it('evaluates simple expressions', () => {
+    var data = {
+      x: 1,
+      y: 2,
+      z: {
+        a: 3
+      }
+    };
+
+    [
+      'x', ' x ', '(x)',
+      'y - 1',
+      'z.a - 2'
+    ]
+    .forEach(expr => {
+      assert.strictEqual(e.evaluate(expr, data), 1);
+    });
+  });
+
+  it('evaluates fat arrow functions', () => {
+    var data = {
+      x: 1,
+      y: 2,
+      z: {
+        a: 3
+      }
+    };
+
+    [
+      'd => d.x',
+      '(d) => d.y - 1',
+      '(d, i) => d.z.a - 2'
+    ]
+    .forEach(expr => {
+      assert.strictEqual(e.evaluator(expr)(data), 1, expr);
+    });
+  });
+
+  it('inherits symbols from the calling context', () => {
+    var data = {
+      x: 1,
+      y: 2,
+      z: {
+        a: 3
+      }
+    };
+
+    var context = {
+      a: 100,
+      b: 200,
+      c: {
+        d: 300
+      }
+    };
+
+    [
+      'x + a',
+      'y + b',
+      'z.a + c.d'
+    ]
+    .forEach(expr => {
+      assert.equal(
+        e.evaluator(expr).call(context, data) % 101,
+        0,
+        expr
+      );
+    });
+  });
+
+});
