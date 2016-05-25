5b521d7e1e0c4318749bb53d2b0179bf08ae6ef0
Shawn Allen <shawn.allen@gsa.gov>
2016-05-24 22:56:08 -0700

    fix some bugs

--- src/property.js
+++ src/property.js
@@ -1,16 +1,24 @@
-module.exports = function property(read, write, value) {
+module.exports = function property(read, write, defaultValue) {
+  var symbol = '[[t-' + Date.now() + ']]';
   return {
     enumerable: false,
 
     get: function() {
-      return read ? read.call(this, value) : value;
+      var value = (symbol in this)
+        ? this[symbol]
+        : this[symbol] = defaultValue;
+      return read
+        ? read.call(this, value)
+        : value;
     },
 
-    set: function(v) {
-      if (v !== value) {
-        var previous = value;
-        return value = write.call(this, v, previous);
+    set: function(value) {
+      var previous = this[symbol];
+      if (value !== previous) {
+        // eslint-disable-next-line no-return-assign
+        return this[symbol] = write.call(this, value, previous);
       }
+      return undefined;
     }
   };
 };
